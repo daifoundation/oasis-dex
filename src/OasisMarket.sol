dc5dc3fe5c14414cbfa48abe41868e6ac2ba1421
Maciek Kamiński <aundumla@gmail.com>
2019-03-19 10:04:33 +0100

    Work in progress

--- src/OasisMarket.sol
+++ src/OasisMarket.sol
@@ -1,4 +1,29 @@
 pragma solidity ^0.5.4;
 
 contract OasisMarket {
+
+	struct Market {
+		ERC20 bidTkn;
+		ERC20 quoteTkn;
+		uint tick;
+		List sellOffers;
+		List buyOffers;
+	}
+
+	mapping (uint => Offer) public offers;
+
+    struct Offer {
+        uint     sellAmt;
+        uint     buyAmt;
+    	uint     price;
+        address  owner;
+        uint64   timestamp;
+    }
+
+    struct Node {
+        uint id;
+        uint prev;
+        uint next;
+    }
+
 }
