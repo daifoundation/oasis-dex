6aa78d3d01481216341bfb2db18c85e4eac9ccb7
Maciek Kamiński <aundumla@gmail.com>
2019-04-05 00:02:08 +0200

    Spring cleaning

--- src/oasis.sol
+++ src/oasis.sol
@@ -5,8 +5,8 @@ import "ds-math/math.sol";
 import "ds-test/test.sol";
 
 contract Oasis is DSTest, DSMath {
-    uint256 private SENTINEL_ID = 0;
-    uint256 private lastOrderId = 0;
+    uint256 private SENTINEL = 0;
+    uint256 private lastId = 0;
 
     struct Order {
         uint256     baseAmt;
@@ -16,17 +16,17 @@ contract Oasis is DSTest, DSMath {
         uint256     next;
     }
 
-	struct Market {
-		ERC20       baseTkn;
-		ERC20       quoteTkn;
-		uint256     dust;
-		uint256     tic;
+    struct Market {
+        ERC20       baseTkn;
+        ERC20       quoteTkn;
+        uint256     dust;
+        uint256     tic;
 
-		mapping (uint256 => Order) sells;
-		mapping (uint256 => Order) buys;
-	}
+        mapping (uint256 => Order) sells;
+        mapping (uint256 => Order) buys;
+    }
 
-	mapping (uint256 => Market) public markets;
+    mapping (uint256 => Market) public markets;
 
     function getMarketId(
         address baseTkn,
@@ -42,136 +42,180 @@ contract Oasis is DSTest, DSMath {
         address quoteTkn,
         uint256 dust,
         uint256 tic
-    ) public returns (uint256 newMarketId) {
-        newMarketId = getMarketId(baseTkn, quoteTkn, dust, tic);
-        Market memory newMarket = Market(ERC20(baseTkn), ERC20(quoteTkn), dust, tic);
-        markets[newMarketId] = newMarket;
+    ) public returns (uint256 id) {
+        id = getMarketId(baseTkn, quoteTkn, dust, tic);
+        // Market memory newMarket = Market(ERC20(baseTkn), ERC20(quoteTkn), dust, tic);
+        markets[id] = Market(ERC20(baseTkn), ERC20(quoteTkn), dust, tic);
     }
 
+    function buy(
+        uint256 mId, uint256 baseAmt, uint256 price, uint256 pos
+    ) public returns (uint256) {
+        return trade(markets[mId], baseAmt, price, true, pos);
+    }
+
+    function sell(
+        uint256 mId, uint256 baseAmt, uint256 price, uint256 pos
+    ) public returns (uint256) {
+        return trade(markets[mId], baseAmt, price, false, pos);
+    }
+
+    // TODO: not tested
+    function cancel(uint256 mId, bool buying, uint256 id) public {
+
+        require(id != SENTINEL, 'sentinele_forever');
+
+        Market storage m = markets[mId];
+        mapping (uint256 => Order) storage orders = buying ? m.buys : m.sells;
+
+        Order storage o = orders[id];
+        require(o.baseAmt > 0, 'no_order');
+        require(msg.sender == o.owner, 'only_owner');
+
+        if(buying) {
+            require(m.baseTkn.transfer(o.owner, o.baseAmt));
+        } else {
+            require(m.quoteTkn.transfer(o.owner, wmul(o.baseAmt, o.price)));
+        }
+
+        remove(orders, id, o);
+
+        return;
+    }
+
+    function getOrder(
+        uint256 mId,
+        bool buying,
+        uint256 orderId
+    ) public view returns (
+        uint256 baseAmt,
+        uint256 price,
+        address owner,
+        uint256 prev,
+        uint256 next
+    ) {
+        Market storage m = markets[mId];
+        Order storage o =  (buying ? m.buys : m.sells)[orderId];
+        return (o.baseAmt, o.price, o.owner, o.prev, o.next);
+    }
+
+    // private methods
+
     function trade(
-        uint256 marketId,
-        uint256 leftBaseAmt,
+        Market storage m,
+        uint256 left,
         uint256 price,
         bool buying,
         uint256 pos
     ) private returns (uint256) {
-
-        Market storage market = markets[marketId];
-
         // dust controll
-        require(wmul(leftBaseAmt, price) >= market.dust, 'dust');
+        require(wmul(left, price) >= m.dust, 'dust');
 
         // tic controll
-        require(price % market.tic == 0, 'tic');
+        require(price % m.tic == 0, 'tic');
 
-        // match with orders on the opposite side of the orderbook
-        mapping (uint256 => Order) storage orders = buying ? market.sells : market.buys;
-
-        (bool notFinal, Order storage current) = first(orders);
-        while(notFinal && (buying ? price >= current.price : price <= current.price)) {
-            if (leftBaseAmt > current.baseAmt) {
-                // complete take
-                uint256 quoteAmt = wmul(current.baseAmt, current.price);
-                swap(market, buying, msg.sender, current.owner, current.baseAmt, quoteAmt);
-                leftBaseAmt -= current.baseAmt;
-                Order storage toBeRemoved = current;
-                (notFinal, current) = getNext(orders, current);
-                remove(orders, toBeRemoved);
-            } else {
-                // partial take
-                uint256 quoteAmt = wmul(leftBaseAmt, current.price);
-                swap(market, buying, msg.sender, current.owner, leftBaseAmt, quoteAmt);
-
-                if(current.baseAmt == leftBaseAmt) {
-                    remove(orders, current);
-                    return 0;
-                }
-
-                current.baseAmt -= leftBaseAmt;
-
-                // dust controll
-                quoteAmt = wmul(current.baseAmt, current.price);
-                if(quoteAmt < market.dust) {
-                    giveUp(market, buying, current.owner, current.baseAmt, quoteAmt);
-                    remove(orders, current);
-                }
+        // limit order matching
+        mapping (uint256 => Order) storage orders = buying ? m.sells : m.buys;
+        uint256 id = orders[SENTINEL].next;
+        Order storage o = orders[id];
+        while(id != SENTINEL && (buying ? price >= o.price : price <= o.price)) {
+            uint256 next = o.next;
+            left = take(m, buying, orders, id, o, left);
+            if(left == 0) {
                 return 0;
             }
-        }
-
-        // 'our' side of the orderbook
-        orders = buying ? market.buys : market.sells;
-
-        // dust controll
-        uint256 quoteAmt = wmul(leftBaseAmt, price);
-        if(quoteAmt < market.dust) {
-            return 0;
+            o = orders[id = next];
         }
 
         // make
-        escrow(market, buying, leftBaseAmt, quoteAmt);
-        Order storage worse = findWorse(orders, buying, price, pos);
-        return insertBefore(orders, worse, leftBaseAmt, price, msg.sender);
+        return make(m, buying, left, price, pos);
     }
 
-    function findWorse(
-        mapping (uint256 => Order) storage orders, bool buying, uint256 price, uint pos
-    ) private view returns (Order storage current) {
-        bool notFinal;
-        if(exists(orders, pos)) {
-            // backtrack if necessary
-            current = getOrder(orders, pos);
-            notFinal = !isFirst(current);
-            while(notFinal && (buying ? current.price < price : current.price > price)) {
-                (notFinal, current) = getPrev(orders, current);
-            }
-            notFinal = true;
+    function take(
+        Market storage m,
+        bool buying,
+        mapping (uint256 => Order) storage orders,
+        uint256 id,
+        Order storage o,
+        uint256 left
+    ) private returns (uint256) {
+        if (left > o.baseAmt) {
+            // complete take
+            uint256 quoteAmt = wmul(o.baseAmt, o.price);
+            swap(m, buying, msg.sender, o.owner, o.baseAmt, quoteAmt);
+            left = left - o.baseAmt;
+            remove(orders, id, o);
+            return left;
         } else {
-            (notFinal, current) = first(orders);
-        }
+            // partial take
+            uint256 quoteAmt = wmul(left, o.price);
+            swap(m, buying, msg.sender, o.owner, left, quoteAmt);
 
-        while(notFinal && (buying ? current.price >= price : current.price <= price)) {
-            (notFinal, current) = getNext(orders, current);
+            if(o.baseAmt == left) {
+                remove(orders, id, o);
+                return 0;
+            }
+
+            left = o.baseAmt - left;
+
+            // dust controll
+            quoteAmt = wmul(left, o.price);
+            if(quoteAmt < m.dust) {
+                // give back
+                if(buying) {
+                    require(m.baseTkn.transfer(o.owner, left));
+                } else {
+                    require(m.quoteTkn.transfer(o.owner, quoteAmt));
+                }
+                remove(orders, id, o);
+            }
+
+            o.baseAmt = left;
+
+            return 0;
         }
     }
 
-    function buy(
-        uint256 marketId, uint256 baseAmt, uint256 price, uint256 pos
-    ) public returns (uint256) {
-        return trade(marketId, baseAmt, price, true, pos);
-    }
-
-    function sell(
-        uint256 marketId, uint256 baseAmt, uint256 price, uint256 pos
-    ) public returns (uint256) {
-        return trade(marketId, baseAmt, price, false, pos);
-    }
-
-    function cancel(uint256 marketId, uint256 orderId) public {
-        Market storage market = markets[marketId];
-        Order storage order = market.sells[orderId];
-
-        if(order.baseAmt > 0) {
-            require(msg.sender == order.owner, 'only_owner');
-            require(market.baseTkn.transfer(order.owner, order.baseAmt));
-            remove(market.sells, order);
-            return;
+    function make(
+        Market storage m,
+        bool buying,
+        uint256 baseAmt,
+        uint256 price,
+        uint pos
+    ) private returns (uint id) {
+        // dust controll
+        uint256 quoteAmt = wmul(baseAmt, price);
+        if(quoteAmt < m.dust) {
+            return 0;
         }
 
-        order = market.buys[orderId];
-        if(order.baseAmt > 0) {
-            require(msg.sender == order.owner, 'only_owner');
-            require(market.quoteTkn.transfer(order.owner, order.baseAmt * order.price));
-            remove(market.buys, order);
-            return;
+        mapping (uint256 => Order) storage orders = buying ? m.buys : m.sells;
+
+        Order storage o = orders[pos];
+        if(o.baseAmt > 0) {
+            // backtrack if necessary
+            while(o.prev != SENTINEL && (buying ? o.price < price : o.price > price)) {
+                o = orders[pos = o.prev]; // previous
+            }
+        } else {
+            o = orders[pos = orders[SENTINEL].next]; // first
+        }
+        while(pos != SENTINEL && (buying ? o.price >= price : o.price <= price)) {
+            o = orders[pos = o.next]; // next
         }
 
-        require(false, 'wrong_order');
+        id = insertBefore(orders, o, baseAmt, price, msg.sender);
+
+        // escrow
+        if(buying) {
+            require(m.quoteTkn.transferFrom(msg.sender, address(this), quoteAmt));
+        } else {
+            require(m.baseTkn.transferFrom(msg.sender, address(this), baseAmt));
+        }
     }
 
-    // transfer helpers
     function swap(
-        Market storage market,
+        Market storage m,
         bool buying,
         address guy1,
         address guy2,
@@ -179,74 +223,23 @@ contract Oasis is DSTest, DSMath {
         uint256 quoteAmt
     ) internal {
         if(buying) {
-            require(market.quoteTkn.transferFrom(guy1, guy2, quoteAmt));
-            require(market.baseTkn.transfer(guy1, baseAmt));
+            require(m.quoteTkn.transferFrom(guy1, guy2, quoteAmt));
+            require(m.baseTkn.transfer(guy1, baseAmt));
         } else {
-            require(market.baseTkn.transferFrom(guy1, guy2, baseAmt));
-            require(market.quoteTkn.transfer(guy1, quoteAmt));
+            require(m.baseTkn.transferFrom(guy1, guy2, baseAmt));
+            require(m.quoteTkn.transfer(guy1, quoteAmt));
         }
-    }
-
-    function giveUp(
-        Market storage market,
-        bool buying,
-        address guy,
-        uint256 baseAmt,
-        uint256 quoteAmt
-    ) internal {
-        if(buying) {
-            require(market.baseTkn.transfer(guy, baseAmt));
-        } else {
-            require(market.quoteTkn.transfer(guy, quoteAmt));
-        }
-    }
-
-    function escrow(
-        Market storage market,
-        bool buying,
-        uint256 baseAmt,
-        uint256 quoteAmt
-    ) internal {
-        if(buying) {
-            require(market.quoteTkn.transferFrom(msg.sender, address(this), quoteAmt));
-        } else {
-            require(market.baseTkn.transferFrom(msg.sender, address(this), baseAmt));
-        }
-    }
-
-    // list helpers
-    function first(
-        mapping (uint256 => Order) storage orders
-    ) internal view returns (bool, Order storage) {
-        uint id = orders[SENTINEL_ID].next;
-        return (id != SENTINEL_ID, orders[id]);
     }
 
     function remove(
         mapping (uint256 => Order) storage orders,
+        uint256 id,
         Order storage order
     ) internal {
-        uint currentId = orders[order.prev].next;
-        require(currentId != SENTINEL_ID);
+        require(id != SENTINEL);
         orders[order.next].prev = order.prev;
         orders[order.prev].next = order.next;
-        delete orders[currentId];
-    }
-
-    function getNext(
-        mapping (uint256 => Order) storage orders,
-        Order storage order
-    ) internal view returns (bool, Order storage) {
-        uint id = order.next;
-        return (id != SENTINEL_ID, orders[id]);
-    }
-
-    function getPrev(
-        mapping (uint256 => Order) storage orders,
-        Order storage order
-    ) internal view returns (bool, Order storage) {
-        uint id = order.prev;
-        return (id != SENTINEL_ID, orders[id]);
+        delete orders[id];
     }
 
     function insertBefore(
@@ -258,58 +251,14 @@ contract Oasis is DSTest, DSMath {
     ) internal returns (uint) {
         require(baseAmt > 0);
         require(price > 0);
-        Order storage newOrder = orders[++lastOrderId];
+        Order storage newOrder = orders[++lastId];
         newOrder.next = orders[order.prev].next;
         newOrder.prev = order.prev;
-        orders[order.prev].next = lastOrderId;
-        order.prev = lastOrderId;
+        orders[order.prev].next = lastId;
+        order.prev = lastId;
         newOrder.owner = owner;
         newOrder.baseAmt = baseAmt;
         newOrder.price = price;
-        return lastOrderId;
-    }
-
-    function isFirst(
-        Order storage order
-    ) internal view returns (bool) {
-        return order.prev == SENTINEL_ID;
-    }
-
-    function isLast(
-        Order storage order
-    ) internal view returns (bool) {
-        return order.next == SENTINEL_ID;
-    }
-
-    function getOrder(
-        mapping (uint256 => Order) storage orders,
-        uint256 id
-    ) internal view returns (Order storage) {
-        Order storage order = orders[id];
-        require((order).baseAmt != 0);
-        return order;
-    }
-
-    function exists(
-        mapping (uint256 => Order) storage orders,
-        uint256 id
-    ) internal view returns (bool) {
-        return orders[id].baseAmt != 0;
-    }
-
-    function getOrderPublic(
-        uint256 marketId,
-        bool buying,
-        uint256 orderId
-    ) public view returns (
-        uint256 baseAmt,
-        uint256 price,
-        address owner,
-        uint256 prev,
-        uint256 next
-    ) {
-        Market storage m = markets[marketId];
-        Order storage o =  (buying ? m.buys : m.sells)[orderId];
-        return (o.baseAmt, o.price, o.owner, o.prev, o.next);
+        return lastId;
     }
 }
