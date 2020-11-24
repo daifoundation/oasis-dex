541938c6023e00422e7d79d6862f03834579cd3d
Maciek Kamiński <aundumla@gmail.com>
2020-11-24 23:30:00 +0100

    Events WIP

--- contracts/OasisEscrowInternalBalances.sol
+++ contracts/OasisEscrowInternalBalances.sol
@@ -4,11 +4,26 @@ pragma solidity >= 0.6.0;
 import "./OasisBase.sol";
 import "./ERC20Like.sol";
 
+
 contract OasisEscrowInternalBalances is OasisBase {
     address  public baseTkn;  // base token
     address  public quoteTkn; // quote token
     mapping (address => uint) public baseBal; // user -> amount
     mapping (address => uint) public quoteBal; // user -> amount
+
+    event Join (
+        bool base,
+        address sender,
+        address usr,
+        uint amt
+    );
+
+    event Exit (
+        bool base,
+        address sender,
+        address usr,
+        uint amt
+    );
 
     constructor(
         address baseTkn_, address quoteTkn_,
@@ -23,13 +38,15 @@ contract OasisEscrowInternalBalances is OasisBase {
             base ? (baseTkn, baseBal) : (quoteTkn, quoteBal);
         require(ERC20Like(tkn).transferFrom(msg.sender, address(this), amt));
         bal[usr] = add(bal[usr], amt);
+        emit Join(base, msg.sender, usr, amt);
     }
 
     function exit(bool base, address usr, uint amt) virtual public {
         (address tkn, mapping (address => uint) storage bal) =
             base ? (baseTkn, baseBal) : (quoteTkn, quoteBal);
-        bal[usr] = sub(bal[usr], amt);
+        bal[msg.sender] = sub(bal[msg.sender], amt);
         require(ERC20Like(tkn).transfer(usr, amt));
+        emit Exit(base, msg.sender, usr, amt);
     }
 
     function escrow(address owner, bool buying, uint amt) internal override {
