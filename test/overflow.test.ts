16d317024db45ade17b7d563e879ebd085847895
Michał Ziętalak <michal.z@ethworks.io>
2020-11-25 13:15:26 +0100

    Use string values in asserts

--- test/overflow.test.ts
+++ test/overflow.test.ts
@@ -5,7 +5,7 @@ import { Erc20, OasisNoEscrow, OasisTester } from '../typechain'
 import { OasisCustomer } from './exchange/oasisCustomer'
 import { OrderBook } from './exchange/orderBook'
 import { loadFixtureAdapter } from './fixtures/loadFixture'
-import { noEscrowMkrDaiFixtureForDustTests } from './fixtures/noEscrow'
+import { noEscrowMkrDaiFixture } from './fixtures/noEscrow'
 import { dai, mkr } from './utils/units'
 
 context('no escrow, erc20 MKR/DAI market / OVERFLOW PROTECTION TEST', () => {
@@ -20,29 +20,29 @@ context('no escrow, erc20 MKR/DAI market / OVERFLOW PROTECTION TEST', () => {
   beforeEach(async () => {
     ;({ baseToken: mkrToken, quoteToken: daiToken, oasis, maker, taker } = await loadFixtureAdapter(
       await ethers.getSigners(),
-    )(noEscrowMkrDaiFixtureForDustTests))
+    )(noEscrowMkrDaiFixture))
     orderBook = new OrderBook(oasis)
     alice = new OasisCustomer(maker, mkrToken, daiToken)
     bob = new OasisCustomer(taker, mkrToken, daiToken)
   })
 
   it('testFailMakeOverflow(passesInOurImplementation)', async () => {
-    await alice.joinMkr(mkr(1))
+    await alice.joinMkr(mkr('1'))
 
-    await alice.sell(mkr(0.99999999999999999), dai(500.01), 0)
+    await alice.sell(mkr('0.9999999999999999'), dai('500.01'), 0)
 
-    expect(await orderBook.sellDepth()).to.be.eql(1)
+    expect(await orderBook.sellDepth()).to.eq(1)
   })
 
   it('testFailTakeOverflow(passesInOurImplementation)', async () => {
-    await alice.joinMkr(mkr(1))
+    await alice.joinMkr(mkr('1'))
 
-    await alice.sell(mkr(1), dai(500), 0)
+    await alice.sell(mkr('1'), dai('500'), 0)
 
-    await bob.joinDai(dai(601))
-    await bob.buy(mkr(0.99999999999999999), dai(600.01), 0)
+    await bob.joinDai(dai('601'))
+    await bob.buy(mkr('0.9999999999999999'), dai('600.01'), 0)
 
-    expect(await bob.mkrDelta()).to.be.eql(mkr(1))
-    expect(await alice.daiDelta()).to.be.eql(dai(500))
+    expect(await bob.mkrDelta()).to.eq(mkr('0.9999999999999999'))
+    expect(await alice.daiDelta()).to.eq(dai('499.99999999999995'))
   })
 })
