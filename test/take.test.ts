8027601a33fdfadae535e8615758d376ede56390
Ewa Kowalska <ewcia.kowalska@gmail.com>
2020-11-25 13:43:35 +0100

    Draft tests 2 fixtures

--- test/take.test.ts
+++ test/take.test.ts
@@ -27,6 +27,7 @@ import { dai, mkr } from './utils/units';
     })
 
     it('testSingleSellComplete', async () => {
+      
       await alice.buy(mkr(1), dai(600), 0)
       await alice.buy(mkr(1), dai(500), 0)
 
@@ -43,15 +44,16 @@ import { dai, mkr } from './utils/units';
       expect(await orderBook.mkrBalance()).to.eq(mkr(0))
 
       expect(await alice.mkrDelta()).to.eq(mkr(1))
+      
       expect(await bob.daiDelta()).to.eq(dai(600))
+      expect(await bob.mkrDelta()).to.eq(mkr(-1))
     })
 
     it('testMultiSellComplete', async () => {
-      await alice.joinDai(dai(1100))
+      
       await alice.buy(mkr(1), dai(600), 0)
       await alice.buy(mkr(1), dai(500), 0)
 
-      await bob.joinMkr(mkr(2))
       const { position } = await bob.sell(mkr(2), dai(500), 0)
 
       expect(position).to.eq(0)
@@ -67,11 +69,10 @@ import { dai, mkr } from './utils/units';
     })
 
     it('testMultiSellCompleteThenMake', async () => {
-      await alice.joinDai(dai(1100))
+      
       await alice.buy(mkr(1), dai(600), 0)
       await alice.buy(mkr(1), dai(500), 0)
 
-      await bob.joinMkr(mkr(3))
       const { position } = await bob.sell(mkr(3), dai(500), 0)
 
       expect(position).to.not.eq(0)
@@ -86,11 +87,9 @@ import { dai, mkr } from './utils/units';
       expect(await alice.mkrDelta()).to.eq(mkr(2))
 
       expect(await bob.daiDelta()).to.eq(dai(1100))
-      expect(await bob.mkrDelta()).to.eq(mkr(-2))
     })
 
     it('testSingleSellIncomplete', async () => {
-      await alice.joinDai(dai(1100))
 
       await alice.buy(mkr(1), dai(600), 0)
       await alice.buy(mkr(1), dai(500), 0)
@@ -99,7 +98,6 @@ import { dai, mkr } from './utils/units';
       expect(await orderBook.sellDepth()).to.eq(0)
       expect(await orderBook.buyDepth()).to.eq(2)
 
-      await bob.joinMkr(mkr(0.5))
       const { position } = await bob.sell(mkr(0.5), dai(600), 0)
       expect(position).to.eq(0) // order immediately filled
       expect(await orderBook.sellDepth()).to.eq(0)
@@ -108,7 +106,6 @@ import { dai, mkr } from './utils/units';
       expect(await orderBook.daiBalance()).to.eq(dai(800))
       expect(await orderBook.mkrBalance()).to.eq(mkr(0))
 
-      expect(await alice.daiDelta()).to.eq(dai(-300))
       expect(await alice.mkrDelta()).to.eq(mkr(0.5))
 
       expect(await bob.daiDelta()).to.eq(dai(300))
@@ -116,12 +113,11 @@ import { dai, mkr } from './utils/units';
     })
 
     it('testMultiSellIncomplete', async () => {
-      await alice.joinDai(dai(1100))
+
       await alice.buy(mkr(1), dai(600), 0)
       const buyOrder = await alice.buy(mkr(1), dai(500), 0)
       expect(await orderBook.daiBalance()).to.eq(dai(1100))
 
-      await bob.joinMkr(mkr(1.5))
       const { position } = await bob.sell(mkr(1.5), dai(500), 0)
 
       expect(position).to.eq(0) // order immediately filled
@@ -135,7 +131,6 @@ import { dai, mkr } from './utils/units';
       expect(await orderBook.daiBalance()).to.eq(dai(250))
       expect(await orderBook.mkrBalance()).to.eq(mkr(0))
 
-      expect(await alice.daiDelta()).to.eq(dai(-850))
       expect(await alice.mkrDelta()).to.eq(mkr(1.5))
 
       expect(await bob.daiDelta()).to.eq(dai(850))
@@ -143,7 +138,6 @@ import { dai, mkr } from './utils/units';
     })
 
     it('testSingleBuyComplete', async () => {
-      await alice.joinMkr(mkr(2))
 
       await alice.sell(mkr(1), dai(500), 0)
       await alice.sell(mkr(1), dai(600), 0)
@@ -151,7 +145,6 @@ import { dai, mkr } from './utils/units';
       expect(await orderBook.sellDepth()).to.eq(2)
       expect(await orderBook.buyDepth()).to.eq(0)
 
-      await bob.joinDai(dai(500))
       await bob.buy(mkr(1), dai(500), 0)
 
       expect(await orderBook.sellDepth()).to.eq(1)
@@ -164,15 +157,12 @@ import { dai, mkr } from './utils/units';
       expect(await alice.mkrDelta()).to.eq(mkr(-1))
 
       expect(await bob.daiDelta()).to.eq(dai(-500))
-      expect(await bob.mkrDelta()).to.eq(mkr(1))
     })
 
     it('testMultiBuyComplete', async () => {
-      await alice.joinMkr(mkr(2))
       await alice.sell(mkr(1), dai(500), 0)
       await alice.sell(mkr(1), dai(600), 0)
 
-      await bob.joinDai(dai(1200))
       const { position } = await bob.buy(mkr(2), dai(600), 0)
 
       expect(position).to.eq(0)
