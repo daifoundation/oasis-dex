1e1dbeb88e631681d938fe8672ff32c8b5b68317
Michał Ziętalak <michal.z@ethworks.io>
2020-12-08 16:42:07 +0100

    Add incomplete fok tests

--- test/fok.test.ts
+++ test/fok.test.ts
@@ -16,11 +16,11 @@ import { dai, mkr } from './utils/units'
       ;({ orderBook, alice, bob} = await loadFixtureAdapter(await ethers.getSigners())(fixture))
     })
     describe('when selling', () => {
-      it('dose not make order when there is no match', async () => {
-        await bob.fokSell(mkr('1'), dai('500'), mkr('1'))
+      // it('does not make order when there is no match', async () => {
+      //   await bob.fokSell(mkr('1'), dai('500'), dai('500'))
 
-        expect(await orderBook.sellDepth()).to.eq(0)
-      })
+      //   expect(await orderBook.sellDepth()).to.eq(0)
+      // })
 
       it('matches single buy order', async () => {
         await alice.buy(mkr('1'), dai('500'), 0)
@@ -39,7 +39,7 @@ import { dai, mkr } from './utils/units'
         expect(await bob.daiDelta()).to.eq(dai("500"))
       })
 
-      it('matches multiple buy order', async () => {
+      it('matches multiple buy orders', async () => {
         await alice.buy(mkr('1'), dai('600'), 0)
         await alice.buy(mkr('1'), dai('500'), 0)
       
@@ -56,14 +56,55 @@ import { dai, mkr } from './utils/units'
         expect(await bob.mkrDelta()).to.eq(mkr("-2"))
         expect(await bob.daiDelta()).to.eq(dai("1100"))
       })
+
+      it('matches buy order incompletely', async () => {
+        await alice.buy(mkr('1'), dai('600'), 0)
+      
+        expect(await orderBook.buyDepth()).to.eq(1)
+
+        await bob.fokSell(mkr('0.5'), dai('500'), mkr('1'))
+  
+        expect(await orderBook.buyDepth()).to.eq(1)
+        expect(await orderBook.sellDepth()).to.eq(0)
+    
+        expect(await orderBook.mkrBalance()).to.eq(mkr('0'))
+        expect(await orderBook.daiBalance()).to.eq(dai('300'))
+
+        expect(await alice.mkrDelta()).to.eq(mkr("0.5"))
+        //expect(await alice.daiDelta()).to.eq(dai("-500"))
+
+        expect(await bob.mkrDelta()).to.eq(mkr("-0.5"))
+        expect(await bob.daiDelta()).to.eq(dai("300"))
+      })
+
+      it('matches multiple buy orders incompletely', async () => {
+        await alice.buy(mkr('1'), dai('600'), 0)
+        await alice.buy(mkr('1'), dai('500'), 0)
+      
+        expect(await orderBook.buyDepth()).to.eq(2)
+
+        await bob.fokSell(mkr('1.5'), dai('500'), mkr('2'))
+  
+        expect(await orderBook.buyDepth()).to.eq(1)
+        expect(await orderBook.sellDepth()).to.eq(0)
+    
+        expect(await orderBook.daiBalance()).to.eq(dai('250'))
+        expect(await orderBook.mkrBalance()).to.eq(mkr('0'))
+
+        expect(await alice.mkrDelta()).to.eq(mkr("1.5"))
+        //expect(await alice.daiDelta()).to.eq(dai("-1100"))
+
+        expect(await bob.mkrDelta()).to.eq(mkr("-1.5"))
+        expect(await bob.daiDelta()).to.eq(dai("850"))
+      })
     })
 
     describe('when buying', () => {
-      it('dose not make order when there is no match', async () => {
-        await bob.fokBuy(mkr('1'), dai('500'), dai('500'))
+      // it('dose not make order when there is no match', async () => {
+      //   await bob.fokBuy(mkr('1'), dai('500'), dai('500'))
         
-        expect(await orderBook.buyDepth()).to.eq(0)
-      })
+      //   expect(await orderBook.buyDepth()).to.eq(0)
+      // })
 
       it('matches single sell order', async () => {
         await alice.sell(mkr('1'), dai('500'), 0)
@@ -82,7 +123,7 @@ import { dai, mkr } from './utils/units'
         expect(await bob.daiDelta()).to.eq(dai("-500"))
       })
 
-      it('matches multiple sell order', async () => {
+      it('matches multiple sell orders', async () => {
         await alice.sell(mkr('1'), dai('600'), 0)
         await alice.sell(mkr('1'), dai('500'), 0)
       
@@ -99,7 +140,45 @@ import { dai, mkr } from './utils/units'
         expect(await bob.mkrDelta()).to.eq(mkr("2"))
         expect(await bob.daiDelta()).to.eq(dai("-1100"))
       })
-    })
+      it('matches sell order incompletely', async () => {
+        await alice.sell(mkr('1'), dai('600'), 0)
+      
+        expect(await orderBook.sellDepth()).to.eq(1)
+
+        await bob.fokBuy(mkr('0.5'), dai('600'), dai('600'))
+  
+        expect(await orderBook.buyDepth()).to.eq(0)
+        expect(await orderBook.sellDepth()).to.eq(1)
     
+        expect(await orderBook.daiBalance()).to.eq(dai('0'))
+        expect(await orderBook.mkrBalance()).to.eq(mkr('0.5'))
+
+        // expect(await alice.mkrDelta()).to.eq(mkr("-1"))
+        expect(await alice.daiDelta()).to.eq(dai("300"))
+
+        expect(await bob.mkrDelta()).to.eq(mkr("0.5"))
+        expect(await bob.daiDelta()).to.eq(dai("-300"))
+      })
+      it('matches multiple sell orders incompletely', async () => {
+        await alice.sell(mkr('1'), dai('600'), 0)
+        await alice.sell(mkr('1'), dai('500'), 0)
+      
+        expect(await orderBook.sellDepth()).to.eq(2)
+
+        await bob.fokBuy(mkr('1.5'), dai('600'), dai('1100'))
+  
+        expect(await orderBook.buyDepth()).to.eq(0)
+        expect(await orderBook.sellDepth()).to.eq(1)
+    
+        expect(await orderBook.daiBalance()).to.eq(dai('0'))
+        expect(await orderBook.mkrBalance()).to.eq(mkr('0.5'))
+
+        // expect(await alice.mkrDelta()).to.eq(mkr("-2"))
+        expect(await alice.daiDelta()).to.eq(dai("800"))
+
+        expect(await bob.mkrDelta()).to.eq(mkr("1.5"))
+        expect(await bob.daiDelta()).to.eq(dai("-800"))
+      })
+    })
   })
 })
