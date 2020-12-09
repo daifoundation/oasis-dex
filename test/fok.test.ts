3164a486c4fb2b9b38df3344d4d760de1789ee10
Michał Ziętalak <michal.z@ethworks.io>
2020-12-09 17:00:59 +0100

    Add fok tests with total limit

--- test/fok.test.ts
+++ test/fok.test.ts
@@ -16,11 +16,11 @@ import { dai, mkr } from './utils/units'
       ;({ orderBook, alice, bob} = await loadFixtureAdapter(await ethers.getSigners())(fixture))
     })
     describe('when selling', () => {
-      // it('does not make order when there is no match', async () => {
-      //   await bob.fokSell(mkr('1'), dai('500'), dai('500'))
+      it('gets reverted when there is no order to match', async () => {
+        await expect(bob.fokSell(mkr('1'), dai('500'), dai('500'))).to.be.reverted
 
-      //   expect(await orderBook.sellDepth()).to.eq(0)
-      // })
+        expect(await orderBook.sellDepth()).to.eq(0)
+      })
 
       it('matches single buy order', async () => {
         await alice.buy(mkr('1'), dai('500'), 0)
@@ -97,14 +97,39 @@ import { dai, mkr } from './utils/units'
         expect(await bob.mkrDelta()).to.eq(mkr("-1.5"))
         expect(await bob.daiDelta()).to.eq(dai("850"))
       })
+      describe('and matching a lower-value buy order', ()=>{
+        it('gets reverted because it does not fit total limit', async () => {
+          await alice.buy(mkr('0.5'), dai('500'), 0)
+  
+          await expect(bob.fokSell(mkr('1'), dai('500'), dai('500'))).to.be.reverted
+  
+          expect(await orderBook.sellDepth()).to.eq(0)
+          expect(await orderBook.buyDepth()).to.eq(1)
+        })
+  
+        it('fits in total limit', async () => {
+          await alice.buy(mkr('0.5'), dai('500'), 0)
+  
+          await bob.fokSell(mkr('1'), dai('500'), dai('200'))
+  
+          expect(await orderBook.sellDepth()).to.eq(0)
+          expect(await orderBook.buyDepth()).to.eq(0)
+  
+          expect(await alice.mkrDelta()).to.eq(mkr("0.5"))
+          expect(await alice.daiDelta()).to.eq(dai("-250"))
+  
+          expect(await bob.mkrDelta()).to.eq(mkr("-0.5"))
+          expect(await bob.daiDelta()).to.eq(dai("250"))
+        })  
+      })
     })
 
     describe('when buying', () => {
-      // it('dose not make order when there is no match', async () => {
-      //   await bob.fokBuy(mkr('1'), dai('500'), dai('500'))
+      it('dose not make order when there is no match', async () => {
+        await bob.fokBuy(mkr('1'), dai('500'), dai('500'))
         
-      //   expect(await orderBook.buyDepth()).to.eq(0)
-      // })
+        expect(await orderBook.buyDepth()).to.eq(0)
+      })
 
       it('matches single sell order', async () => {
         await alice.sell(mkr('1'), dai('500'), 0)
@@ -178,6 +203,31 @@ import { dai, mkr } from './utils/units'
 
         expect(await bob.mkrDelta()).to.eq(mkr("1.5"))
         expect(await bob.daiDelta()).to.eq(dai("-800"))
+      })
+      describe('and matching a lower-value sell order', ()=> {
+        it('gets reverted because it does not fit total limit', async () => {
+          await alice.sell(mkr('0.5'), dai('500'), 0)
+  
+          await expect(bob.fokBuy(mkr('1'), dai('500'), dai('200'))).to.be.reverted
+  
+          expect(await orderBook.sellDepth()).to.eq(1)
+          expect(await orderBook.buyDepth()).to.eq(0)
+        })
+
+        it('fits total limit', async () => {
+          await alice.sell(mkr('0.5'), dai('500'), 0)
+  
+          await expect(bob.fokBuy(mkr('1'), dai('500'), dai('500'))).not.to.be.reverted
+  
+          expect(await orderBook.sellDepth()).to.eq(0)
+          expect(await orderBook.buyDepth()).to.eq(0)
+
+          expect(await alice.mkrDelta()).to.eq(mkr("-0.5"))
+          expect(await alice.daiDelta()).to.eq(dai("250"))
+
+          expect(await bob.mkrDelta()).to.eq(mkr("0.5"))
+          expect(await bob.daiDelta()).to.eq(dai("-250"))
+        })        
       })
     })
   })
